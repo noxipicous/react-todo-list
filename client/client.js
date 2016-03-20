c0085f2364e62eee43b0d364903e38f50e15fdd0
noxipicous <noxipicous@gmail.com>
2016-03-20 00:42:59 -0400

    got it rendering component

--- client/client.js
+++ client/client.js
@@ -1,1 +1,11 @@
+import React from 'react';
+import {render} from 'react-dom';
+import App from '../components/App';
 
+
+render(
+    // define componenet
+    // DOM Element we're mounting on
+    <App/>,
+    document.getElementById('app')
+)
